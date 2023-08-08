55d832953fef297f562c484a47d2907a356983b3
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-08-08 07:53:50 +0000

    Update script.js
    Create functions to store and read book data

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-const button = document.querySelector('button');
-const title = document.querySelector('.title');
-const author = document.querySelector('.author');
-const content = document.querySelector('.books');
-const list = document.querySelector('.list');
-const add = document.querySelector('.add');
-const contact = document.querySelector('.contact');
-const books = document.querySelector('.books');
-const form = document.querySelector('.form-item');
+// Checking if local storage is empty then add an empty array
+if (localStorage.getItem("Added Books") == null) {
+    localStorage.setItem("Added Books", JSON.stringify([]));
+  }
+  
+  // store data into local storage
+  const storeData = JSON.parse(localStorage.getItem("Added Books"));
+  
+  function updateData() {
+    localStorage.setItem("Added Books", JSON.stringify(storeData));
+  }
+  
+  // Getting values from input fields
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    const title = document.querySelector(".title");
+    const author = document.querySelector(".author");
+    e.preventDefault();
+    addNewdata(title.value, author.value);
+  });
+  
+  function createBooks(arr) {
+    let books = "";
+    for (let i = 0; i < arr.length; i += 1) {
+      books += `
+          ${arr[i].title}<br>
+          ${arr[i].author}<br>
+          <button onclick="removeBook('${i}')">Remove</button>
+          <hr/>
+          `;
+    }
+    return books;
+  }
+  
