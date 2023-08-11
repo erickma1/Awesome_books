b87bb762115ce73189dae2f51847f4c90ebb4a25
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-08-11 14:06:12 +0000

    Update script.js
    Debug the function to add book

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ class BookManager {
          <article class='books-container'>
          ${this.storeData[i].title} by
           ${this.storeData[i].author}<br>
-          <button class='remove-btn' onclick='bookManager.removeBook('${i}')'>Remove</button>
+          <button class='remove-btn' onclick="bookManager.removeBook('${i}')">Remove</button>
           </article>
           </section>
         `;
