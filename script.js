8924c5022d0763390ece9fb8db4cde075b64622f
brighton0102 <mtandibrighton0102@gmail.com>
2023-08-11 14:12:44 +0200

    Show active nav link

--- script.js
+++ script.js
@@ -124,3 +124,19 @@ switch (day) {
 output.innerHTML = day;
 date.innerHTML = ndate;
 
+/* Visited menu*/
+
+var links = document.querySelectorAll(".links li");
+
+links.forEach(li => {
+  li.addEventListener("click",()=> {
+    resetLinks();
+    li.classList.add("active");
+  })
+})
+
+function resetLinks() {
+  links.forEach(li => {
+    li.classList.remove("active")
+  })
+}
