592ae92d19336e305dd93482d6074e660a9450de
erickma1 <110662611+erickma1@users.noreply.github.com>
2023-08-11 10:32:27 +0000

    Update script.js
    add sections created to javascript file

--- script.js
+++ script.js
@@ -4,11 +4,18 @@ class BookManager {
     this.storeData = JSON.parse(localStorage.getItem(this.storeKey)) || [];
     this.form = document.querySelector('form');
     this.listOfBooks = document.querySelector('.container');
+    this.list = document.getElementById('list');
+    this.addNew = document.getElementById('addNew');
+    this.contact = document.getElementById('contact');
+    this.addNewSction = document.getElementById('add-book');
+    this.listSection = document.getElementById('list-books');
+    this.contactSection = document.getElementById('contact-container');
 
     this.form.addEventListener('submit', (e) => {
       e.preventDefault();
       this.addNewData();
     });
+//methods for display
 
     this.displayBooks();
   }
@@ -21,12 +28,12 @@ class BookManager {
     let books = '';
     for (let i = 0; i < this.storeData.length; i += 1) {
       books += `
-         <section id="books">
-         <article class="books-container ">
+         <section id='books'>
+         <article class='books-container'>
          ${this.storeData[i].title} by
           ${this.storeData[i].author}<br>
-          <button class="remove-btn" onclick="bookManager.removeBook('${i}')">Remove</button>
-          <article/>
+          <button class='remove-btn' onclick='bookManager.removeBook('${i}')'>Remove</button>
+          </article>
           </section>
         `;
     }
